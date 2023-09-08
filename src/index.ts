69a4b6d0dfc141103b4f6f352c861749fdff76e1
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-09-08 04:06:30 -0500

    using standalone server now

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,52 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
+import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs, resolvers } from './graphql';
 import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 4000;
+const prisma = new PrismaClient();
 
-const bootstrapServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers
-  });
-  await server.start();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use('/graphql', expressMiddleware(server));
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
+const startServer = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
   });
 
-  app.listen(port, () => {
-    console.log(`🚀 Express ready at http://localhost:${port}`);
-    console.log(`🚀 Graphql ready at http://localhost:${port}/graphql`);
-  });
+  console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Graphql ready at ${url}graphql`);
 };
 
-bootstrapServer();
+startServer();
+
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// const bootstrapServer = async () => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers
+//   });
+//   await server.start();
+
+//   app.use(cors());
+//   app.use(express.json());
+//   app.use(express.urlencoded({ extended: true }));
+//   app.use('/graphql', expressMiddleware(server));
+
+//   app.get('/', (req, res) => {
+//     res.send('Hello World!');
+//   });
+
+//   app.listen(port, () => {
+//     console.log(`🚀 Express ready at http://localhost:${port}`);
+//     console.log(`🚀 Graphql ready at http://localhost:${port}/graphql`);
+//   });
+// };
+
+// bootstrapServer();
