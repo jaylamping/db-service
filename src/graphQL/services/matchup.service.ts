aeb908f4f87ef54a93490a590fac1d5d374fe38e
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-09-11 00:20:52 -0500

    createMatchup getting there

---
+++ src/graphQL/services/matchup.service.ts
@@ -0,0 +1,84 @@
+import { PrismaClient } from '@prisma/client';
+import { extractSelection } from '../utils/extractSelections';
+import { GraphQLResolveInfo } from 'graphql';
+
+const prisma = new PrismaClient();
+
+interface GetMatchupsArgs {
+  info: GraphQLResolveInfo;
+}
+
+interface GetMatchupArgs extends GetMatchupsArgs {
+  id: string;
+}
+
+interface MatchupInput {
+  name: string;
+  description?: string;
+  dateTime: string | Date;
+  homeTeam: string;
+  awayTeam: string;
+  logo_url?: string;
+  league: string;
+  sport: string;
+  primaryStream?: string;
+  secondaryStream?: string;
+  tertiaryStream?: string;
+  fallbackStream?: string;
+}
+
+export const getMatchups = async ({ info }: GetMatchupsArgs) => {
+  const extractedSelections = extractSelection(info);
+  const leagueInd = extractedSelections.includes('league');
+  const sportInd = extractedSelections.includes('sport');
+
+  return await prisma.matchup.findMany({ include: { League: leagueInd, Sport: sportInd } });
+};
+
+export const getMatchup = async ({ id, info }: GetMatchupArgs) => {
+  const extractedSelections = extractSelection(info);
+  const leagueInd = extractedSelections.includes('league');
+  const sportInd = extractedSelections.includes('sport');
+
+  return await prisma.matchup.findUnique({ where: { id }, include: { League: leagueInd, Sport: sportInd } });
+};
+
+export const createMatchup = async ({
+  name,
+  description,
+  dateTime,
+  homeTeam,
+  awayTeam,
+  logo_url,
+  sport,
+  league,
+  primaryStream,
+  secondaryStream,
+  tertiaryStream,
+  fallbackStream
+}: MatchupInput) => {
+  return await prisma.matchup.create({
+    data: {
+      name,
+      description,
+      dateTime,
+      homeTeam,
+      awayTeam,
+      logo_url,
+      sport,
+      league,
+      primaryStream,
+      secondaryStream,
+      tertiaryStream,
+      fallbackStream
+    }
+  });
+};
+
+export const updateTeam = async ({ id, data }: { id: string; data: MatchupInput }) => {
+  return await prisma.team.update({ where: { id }, data });
+};
+
+export const deleteTeam = async ({ id }: { id: string }) => {
+  return await prisma.team.delete({ where: { id } });
+};
