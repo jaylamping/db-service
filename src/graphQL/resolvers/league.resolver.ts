ab3a1c51c45d936ebd107ca608cf67461a2ffb6b
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-09-10 22:16:46 -0500

    league resolver added

---
+++ src/graphQL/resolvers/league.resolver.ts
@@ -0,0 +1,27 @@
+import { GraphQLResolveInfo } from 'graphql';
+import { createLeague, deleteLeague, updateLeague, getLeague, getLeagues } from '../services/league.service';
+
+export const sportResolver = {
+  Query: {
+    leagues: async (parent: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) => {
+      return await getLeagues({ info });
+    },
+    league: async (parent: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) => {
+      return await getLeague({ id: args.id, info });
+    }
+  },
+  Mutation: {
+    createLeague: async (parent: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) => {
+      return await createLeague({ name: args.input.name, logo_url: args.input.logo_url, sportId: args.input.sportId });
+    },
+    updateLeague: async (parent: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) => {
+      return await updateLeague({
+        id: args.input.id,
+        data: { name: args.input.name, logo_url: args.input.logo_url, sportId: args.input.sportId }
+      });
+    },
+    deleteLeague: async (parent: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) => {
+      return await deleteLeague({ id: args.input.id });
+    }
+  }
+};
