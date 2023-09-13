aadffabee25389a91053acc2be40aefa4c444d3a
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-09-13 18:20:40 -0500

    seed updated

--- src/seed.ts
+++ src/seed.ts
@@ -25,4 +25,26 @@ const populateNFLTeams = async () => {
   });
 };
 
-populateNFLTeams();
+const populateMLBTeams = async () => {
+  const teams = await axios
+    .get(
+      `https://www.thesportsdb.com/api/v1/json/${process.env.DATADB_API_KEY}/lookup_all_teams.php?id=${process.env.DATADB_MLB_ID}`
+    )
+    .then(response => {
+      return response.data.teams;
+    });
+
+  teams.map((team: any) => {
+    createTeam({
+      name: team.strTeam,
+      location: team.strStadiumLocation,
+      logo_url: team.strTeamBadge,
+      sport: 'Baseball',
+      league: 'MLB',
+      externalId: team.idTeam
+    });
+  });
+};
+
+//populateNFLTeams();
+//populateMLBTeams();
